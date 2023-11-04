8bf25334fa261386ed7b7d1a2df690d2b9aadf8d
CheolWoong Choi <bluesky4381@naver.com>
2023-11-04 22:10:59 +0900

    setting : init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CustomersPage() {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DashboardPage() {
+  return <p>Dashboard Page</p>;
+}
