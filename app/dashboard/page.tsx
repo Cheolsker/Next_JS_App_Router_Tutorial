8dd0c58a84b2937c705ff433a62e1dd0502308d9
CheolWoong Choi <bluesky4381@naver.com>
2023-11-11 17:37:43 +0900

    Ch 9 : LatestInvoices 컴포넌트도 스트리밍 처리

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,20 +2,20 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const [
-    latestInvoices,
-    {
-      numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    },
-  ] = await Promise.all([fetchLatestInvoices(), fetchCardData()]);
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
 
   /* Parallel 요청 */
   // const [
@@ -62,7 +62,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -24,7 +24,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log("Data fetch complete after 3 seconds.");
+    console.log("Data (Revenue) fetch complete after 3 seconds.");
 
     return data.rows;
   } catch (error) {
@@ -37,12 +37,17 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    // console.log("Fetching LatestInvoices data...");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    // console.log("Data (LatestInvoices) fetch complete after 2 seconds.");
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
