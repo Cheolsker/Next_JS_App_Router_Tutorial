2c2b7eaec9b2ebf4166ec4462eabfa66cb3950dc
CheolWoong Choi <bluesky4381@naver.com>
2023-11-11 17:23:21 +0900

    Ch 9 : Suspense를 이용한 컴포넌트 스트리밍

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,12 +2,12 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  /* Parallel 요청 */
   const [
-    revenue,
     latestInvoices,
     {
       numberOfCustomers,
@@ -15,11 +15,23 @@ export default async function Page() {
       totalPaidInvoices,
       totalPendingInvoices,
     },
-  ] = await Promise.all([
-    fetchRevenue(),
-    fetchLatestInvoices(),
-    fetchCardData(),
-  ]);
+  ] = await Promise.all([fetchLatestInvoices(), fetchCardData()]);
+
+  /* Parallel 요청 */
+  // const [
+  //   revenue,
+  //   latestInvoices,
+  //   {
+  //     numberOfCustomers,
+  //     numberOfInvoices,
+  //     totalPaidInvoices,
+  //     totalPendingInvoices,
+  //   },
+  // ] = await Promise.all([
+  //   fetchRevenue(),
+  //   fetchLatestInvoices(),
+  //   fetchCardData(),
+  // ]);
 
   /* Waterfall 요청 */
   // const revenue = await fetchRevenue();
@@ -47,7 +59,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
