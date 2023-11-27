8dbbcf75451a6900e4b98d3c28cb0418aed33555
CheolWoong Choi <bluesky4381@naver.com>
2023-11-27 11:11:24 +0900

    Ch 16 : 루트 디렉터리에 메타데이터 제목 템플릿을 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Acme Dashboard",
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
   description: "The official Next.js Course Dashboard, built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 };
